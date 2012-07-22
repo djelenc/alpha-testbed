0189e20baaf3416b583a9ad8e3226066ff59b05b
David Jelenc <david.jelenc@fri.uni-lj.si>
2012-07-23 01:19:29 +0200

    Added an example of a Java-programmed run

---
+++ testbed/src/testbed/repast/ProgramaticRun.java
@@ -0,0 +1,38 @@
+package testbed.repast;
+
+import testbed.AlphaTestbed;
+import testbed.interfaces.IRankingMetric;
+import testbed.interfaces.IScenario;
+import testbed.interfaces.ITrustModel;
+import testbed.metric.Accuracy;
+import testbed.scenario.Transitive;
+import testbed.trustmodel.YuSinghSycara;
+
+public class ProgramaticRun {
+
+    public static void main(String[] args) {
+	// trust model
+	ITrustModel model = new YuSinghSycara();
+	model.initialize();
+
+	// scenario
+	IScenario scenario = new Transitive();
+	scenario.initialize(100, 0.05, 0.1, 1d, 1d);
+
+	// ranking metric
+	IRankingMetric rm = new Accuracy();
+	rm.initialize();
+
+	// alpha test-bed (utility metric in set to null)
+	final AlphaTestbed atb = new AlphaTestbed(model, scenario, rm, null);
+
+	for (int time = 1; time <= 300; time++) {
+	    atb.step(time);
+
+	    for (int service : scenario.getServices()) {
+		System.out.printf("%d: %s[%d] = %.2f\n", time, rm, service,
+			atb.getMetric(0, rm));
+	    }
+	}
+    }
+}
