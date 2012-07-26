0354e3fa1a2af793283636dbb1c66f54f8552725
David Jelenc <david.jelenc@fri.uni-lj.si>
2012-07-26 16:42:06 +0200

    Updated ProgramaticRun (and another post-receive hook test)

--- testbed/src/testbed/repast/ProgramaticRun.java
+++ testbed/src/testbed/repast/ProgramaticRun.java
@@ -1,7 +1,9 @@
 package testbed.repast;
 
 import testbed.AlphaTestbed;
+import testbed.IMetricSubscriber;
 import testbed.common.DefaultRandomGenerator;
+import testbed.interfaces.IMetric;
 import testbed.interfaces.IRankingMetric;
 import testbed.interfaces.IScenario;
 import testbed.interfaces.ITrustModel;
@@ -9,7 +11,22 @@ import testbed.metric.Accuracy;
 import testbed.scenario.Transitive;
 import testbed.trustmodel.YuSinghSycara;
 
-public class ProgramaticRun {
+/**
+ * A class that demonstrates how an evaluation can be run as a simple Java
+ * program.
+ * 
+ * @author David
+ * 
+ */
+public class ProgramaticRun implements IMetricSubscriber {
+
+    private final int service;
+    private final IMetric metric;
+
+    public ProgramaticRun() {
+	service = 0;
+	metric = new Accuracy();
+    }
 
     public static void main(String[] args) {
 	// trust model
@@ -29,13 +46,14 @@ public class ProgramaticRun {
 	// alpha test-bed (utility metric in set to null)
 	final AlphaTestbed atb = new AlphaTestbed(scenario, model, rm, null);
 
-	for (int time = 1; time <= 300; time++) {
-	    atb.step(time);
+	atb.subscribe(new ProgramaticRun());
 
-	    for (int service : scenario.getServices()) {
-		System.out.printf("%d: %s[%d] = %.2f\n", time, rm, service,
-			atb.getMetric(0, rm));
-	    }
-	}
+	for (int time = 1; time <= 300; time++)
+	    atb.step(time);
+    }
+
+    @Override
+    public void update(AlphaTestbed instance) {
+	System.out.println(instance.getMetric(service, metric));
     }
 }
