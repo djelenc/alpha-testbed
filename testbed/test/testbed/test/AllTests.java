01e21fa7ab0b9ce20881c47c6f58d995c2940eb5
David Jelenc <david.jelenc@fri.uni-lj.si>
2012-08-14 14:27:34 +0200

    Removed EigenTrustContinuous trust model (with tests)

--- testbed/test/testbed/test/AllTests.java
+++ testbed/test/testbed/test/AllTests.java
@@ -7,13 +7,12 @@ import org.junit.runners.Suite.SuiteClasses;
 @RunWith(Suite.class)
 @SuiteClasses({ UtilsTest.class, DeceptionModelsTest.class,
 	ClassLoadersTest.class, AlphaTestbedTest.class, MetricsTest.class,
-	RandomScenarioTest.class, EigenTrustContinuousTMTest.class,
-	ARHTMTest.class, OnlyExperiencesTMTest.class, SimpleTMTest.class,
-	OnlyOpinionsTMTest.class, BetaReputationTMTest.class,
-	BRSWithFilteringTMTest.class, TravosTMTest.class,
-	YuSinghSycaraTMTest.class, QADTMTest.class, QTMTest.class,
-	OscillationScenarioTest.class, DefaultRandomGeneratorTest.class,
-	ExampleGeneratorTest.class })
+	RandomScenarioTest.class, ARHTMTest.class, OnlyExperiencesTMTest.class,
+	SimpleTMTest.class, OnlyOpinionsTMTest.class,
+	BetaReputationTMTest.class, BRSWithFilteringTMTest.class,
+	TravosTMTest.class, YuSinghSycaraTMTest.class, QADTMTest.class,
+	QTMTest.class, OscillationScenarioTest.class,
+	DefaultRandomGeneratorTest.class, ExampleGeneratorTest.class })
 public class AllTests {
 
 }
