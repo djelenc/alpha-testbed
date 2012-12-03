44136a6b59afd66b73492eee031d5e7def23359d
David Jelenc <david.jelenc@fri.uni-lj.si>
2012-12-03 17:42:48 +0100

    Changed KTA implementation

--- testbed/src/testbed/metric/KendallsTauA.java
+++ testbed/src/testbed/metric/KendallsTauA.java
@@ -24,27 +24,29 @@ public class KendallsTauA extends AbstractMetric implements Accuracy {
 
 	int concordant = 0, discordant = 0;
 
-	for (Entry<Integer, T> rank1 : trust.entrySet()) {
-	    for (Entry<Integer, T> rank2 : trust.entrySet()) {
-		if (rank1.getKey() < rank2.getKey()) {
-		    final T r1 = rank1.getValue();
-		    final T r2 = rank2.getValue();
-		    final Double c1 = capabilities.get(rank1.getKey());
-		    final Double c2 = capabilities.get(rank2.getKey());
+	for (Entry<Integer, Double> cap1 : capabilities.entrySet()) {
+	    for (Entry<Integer, Double> cap2 : capabilities.entrySet()) {
+		if (cap1.getKey() < cap2.getKey()) {
+		    final Double c1 = cap1.getValue();
+		    final Double c2 = cap2.getValue();
+		    final T r1 = trust.get(cap1.getKey());
+		    final T r2 = trust.get(cap2.getKey());
 
-		    final int rankDiff = r1.compareTo(r2);
-		    final int capDiff = c1.compareTo(c2);
+		    if (r1 != null && r2 != null) {
+			final int rankDiff = r1.compareTo(r2);
+			final int capDiff = c1.compareTo(c2);
 
-		    if (rankDiff * capDiff > 0) {
-			concordant++;
-		    } else if (rankDiff * capDiff < 0) {
-			discordant++;
+			if (rankDiff * capDiff > 0) {
+			    concordant++;
+			} else if (rankDiff * capDiff < 0) {
+			    discordant++;
+			}
 		    }
 		}
 	    }
 	}
 
-	final double n = trust.size() * (trust.size() - 1d) / 2d;
+	final double n = capabilities.size() * (capabilities.size() - 1d) / 2d;
 	final double metric = (concordant - discordant) / n;
 
 	return (metric + 1d) / 2d;
