a63cd9e94343db580aab67e26ea8c9f028a2bb80
David Jelenc <david.jelenc@fri.uni-lj.si>
2018-03-04 20:45:41 +0100

    Reads duration form GUI and also simplifies it

--- src/main/kotlin/testbed/app/AppController.kt
+++ src/main/kotlin/testbed/app/AppController.kt
@@ -18,48 +18,59 @@ import testbed.interfaces.*
 import java.util.concurrent.Executors
 
 class AppController : MetricSubscriber {
+    @FXML
+    lateinit var input: TextField
+    @FXML
+    lateinit var init: Button
+    @FXML
+    lateinit var stop: Button
+    @FXML
+    lateinit var verticalBox: VBox
 
-    @FXML
-    var input: TextField? = null
-    @FXML
-    var start: Button? = null
-    @FXML
-    var init: Button? = null
-    @FXML
-    var stop: Button? = null
-    @FXML
-    var vbox: VBox? = null
+    private val chart = LineChart(
+            NumberAxis().apply {
+                tickUnit = 25.0
+                lowerBound = 0.0
+            },
+            NumberAxis().apply {
+                upperBound = 1.0
+                lowerBound = 0.0
+                tickUnit = 0.05
+                isAutoRanging = false
+            })
 
-    private val chart = LineChart(NumberAxis(), NumberAxis())
     private val metricData = HashMap<Metric, XYChart.Series<Number, Number>>()
 
     private val executor = Executors.newSingleThreadExecutor()
 
     @FXML
-    protected fun initialize() {
-        vbox!!.children.add(chart)
-        input?.text = "1"
+    fun initialize() {
+        verticalBox.apply {
+            children += chart
+        }
+        input.text = "1"
     }
 
     fun initButton(event: ActionEvent) {
         val gui = ParametersGUI(AppController::class.java.classLoader)
-        gui.setBatchRun(false)
+        gui.setBatchRun(true)
 
         val answer = gui.showDialog()
 
-        if (answer != 0) return
+        if (answer != 0) {
+            return
+        }
 
-        // seed
-        val seed = input?.text?.let { Integer.parseInt(it) } ?: 1
-        // set scenario
+        val seed = Integer.parseInt(input.text)
+
         val scenario = gui.setupParameters[0] as Scenario
         scenario.setRandomGenerator(DefaultRandomGenerator(seed))
         scenario.initialize(*gui.scenarioParameters)
-        // set trust model
+
         val trustModel = gui.setupParameters[1] as TrustModel<*>
         trustModel.setRandomGenerator(DefaultRandomGenerator(seed))
         trustModel.initialize(*gui.trustModelParameters)
-        // metrics
+
         val metrics = HashMap<Metric, Array<Any>>()
         gui.setupParameters[2]?.let {
             metrics[it as Accuracy] = gui.accuracyParameters
@@ -77,17 +88,17 @@ class AppController : MetricSubscriber {
             chart.data.add(metricData[it])
         }
 
+        val duration = gui.setupParameters[5] as Int
+
         val protocol = AlphaTestbed.getProtocol(trustModel, scenario, metrics)
+        protocol.subscribe(this)
+
         executor.execute {
-            protocol.subscribe(this)
-            for (time in 1..500) {
+            for (time in 1..duration) {
                 protocol.step(time)
-                Thread.yield()
             }
         }
     }
-    
-    fun startButton(event: ActionEvent) {}
 
     fun stopButton(event: ActionEvent) {
         chart.data.clear()
