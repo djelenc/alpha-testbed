f8a0d3998ddfae35008f4323ac0204e998ef517e
David Jelenc <david.jelenc@fri.uni-lj.si>
2019-06-10 12:15:29 +0200

    Renames BRS with filtering

--- src/main/java/atb/trustmodel/BRSWithFiltering.java
+++ src/main/java/atb/trustmodel/BRSWithFiltering.java
@@ -308,7 +308,7 @@ public class BRSWithFiltering extends AbstractTrustModel<Double> {
 
     @Override
     public String toString() {
-        return "BRS with filtering";
+        return "Beta with filtering";
     }
 
     @Override
